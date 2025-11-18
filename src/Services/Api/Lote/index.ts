886a713c70c4e7460e58f1e907d6c3ab6fb77344
Gabriel Christyan <gabriel.gclc@gmail.com>
2025-11-18 11:05:42 -0300

    :sparkles: FEAT: Service de lote completo

---
+++ src/Services/Api/Lote/index.ts
@@ -0,0 +1,47 @@
+import { Api, BaseApiResponse } from "../Axios-Config";
+
+export interface ILote {
+    numeroLote: string;
+    quantidade: number;
+    valorLoteCompra: number;
+    dataCompra: string;
+    dataValidade: string;
+}
+
+// MESMA INTERFACE MAS SEM O ID
+export interface ILotePOST extends Omit<ILote, "id"> { }
+
+const ENTIDADE_API = "Lote";
+
+export type ILoteGET = BaseApiResponse<ILote>
+const listarLotes = async (): Promise<ILoteGET | Error> => {
+    try {
+        const { data } = await Api.get(ENTIDADE_API);
+
+        return data;
+    } catch (error: any) {
+        return error;
+    }
+}
+
+const cadastrarLote = async (lote: ILotePOST) => {
+    try {
+        await Api.post(ENTIDADE_API, lote);
+    } catch (error) {
+        return error;
+    }
+};
+
+const editarLote = async (id: any, lote: ILotePOST) => {
+    try {
+        await Api.put(`${ENTIDADE_API}/${id}`, lote);
+    } catch (error) {
+        return error;
+    }
+};
+
+export const LoteService = {
+    listarLotes,
+    cadastrarLote,
+    editarLote
+}
