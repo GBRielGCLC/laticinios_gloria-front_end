510c8c9012322bfb8e0e5deca91e3d7ffabb1a76
Gabriel Christyan <gabriel.gclc@gmail.com>
2025-12-23 10:55:04 -0300

    :construction: FEAT: Trabalhando num tema compartilhado entre o dark e light, para poder só adicionar ou alterar o que precisa

---
+++ src/Themes/Base.ts
@@ -0,0 +1,53 @@
+import { createTheme } from "@mui/material";
+
+export const BaseTheme = createTheme({
+    typography: {
+        fontFamily: 'system-ui, -apple-system, sans-serif',
+        h1: { fontSize: '2rem', fontWeight: 500 },
+        h2: { fontSize: '1.5rem', fontWeight: 500 },
+        h3: { fontSize: '1.25rem', fontWeight: 500 },
+        h4: { fontSize: '1rem', fontWeight: 500 },
+    },
+    shape: {
+        borderRadius: 10,
+    },
+    components: {
+        MuiFormLabel: {
+            styleOverrides: {
+                asterisk: { color: '#d32f2f' },
+            },
+        },
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    textTransform: 'none',
+                    fontWeight: 500,
+                    borderRadius: 10,
+                },
+            },
+        },
+        MuiCard: {
+            styleOverrides: {
+                root: {
+                    boxShadow: '0 1px 3px 0 rgb(0 0 0 / 0.1)',
+                },
+            },
+        },
+        // A partir daqui não tem na tipagem original do mui
+        //@ts-expect-error
+        MuiDatePicker: {
+            defaultProps: {
+                slotProps: {
+                    actionBar: {
+                        actions: ["today", "clear"],
+                    },
+                },
+            },
+        },
+        MuiDataGrid: {
+            styleOverrides: {
+                borderRadius: 8,
+            },
+        },
+    },
+});
